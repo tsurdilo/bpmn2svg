6a9c93c47c1065fe2dc88101a6a840f2bb3f2154
Tihomir Surdilovic <tsurdilo@redhat.com>
2016-07-26 14:36:49 -0400

    update to test util

--- src/test/java/org/tsurdilo/util/TestUtil.java
+++ src/test/java/org/tsurdilo/util/TestUtil.java
@@ -1,30 +1,20 @@
 package org.tsurdilo.util;
 
-import bpsim.impl.BpsimPackageImpl;
 import org.eclipse.bpmn2.*;
 import org.eclipse.bpmn2.Process;
-import org.jboss.drools.impl.DroolsPackageImpl;
-import org.jbpm.designer.web.profile.IDiagramProfile;
-import org.jbpm.designer.web.profile.impl.DefaultProfileImpl;
+import org.jbpm.designer.web.profile.impl.JbpmProfileImpl;
 
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class TestUtil {
-    DefaultProfileImpl profile = new DefaultProfileImpl();
-    IDiagramProfile.IDiagramUnmarshaller marshaller = profile.createUnmarshaller();
-    IDiagramProfile.IDiagramMarshaller unmarshaller = profile.createMarshaller();
 
     public Definitions getDefinitionsFromBpmn2(Class testClass, String fileName) throws Exception {
         URL fileURL = testClass.getResource(fileName);
         String bpmn2str = new String(Files.readAllBytes(Paths.get(fileURL.toURI())));
 
-        DroolsPackageImpl.init();
-        BpsimPackageImpl.init();
-        String processJson = marshaller.parseModel(bpmn2str, profile, "Email,HelloWorkItemHandler,Log,Rest,WebService");
-
-        return unmarshaller.getDefinitions(processJson, "Email,HelloWorkItemHandler,Log,Rest,WebService");
+       return new JbpmProfileImpl().getDefinitions(bpmn2str);
     }
 
     public org.eclipse.bpmn2.Process getRootProcess(Definitions def) {
